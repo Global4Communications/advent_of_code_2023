0e5cd3858e81f3fe5d893f442fc815c0792e9041
josh.slater <josh.slater@global4.co.uk>
2023-12-05 10:02:41 +0000

    day 1 solved

---
+++ solutions/josh/day_1_josh.php
@@ -0,0 +1,29 @@
+<?php
+
+$calibrationDocument = file_get_contents("day_1_calibration_input.txt");
+$calibrationDocument = trim($calibrationDocument);
+
+$lines = explode("\n", $calibrationDocument);
+
+$sum = 0;
+
+foreach ($lines as $line) {
+    $firstDigit = null;
+    $lastDigit = null;
+
+    for ($i = 0; $i < strlen($line); $i++) {
+        $char = $line[$i];
+
+        if (is_numeric($char)) {
+            if ($firstDigit === null) {
+                $firstDigit = (int)$char;
+            }
+            $lastDigit = (int)$char;
+        }
+    }
+
+    $calibrationValue = $firstDigit * 10 + $lastDigit;
+    $sum += $calibrationValue;
+}
+
+echo $sum;
